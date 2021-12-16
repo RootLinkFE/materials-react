84979ad0ff3a38c539c73793211e4f1dec61ffe8
giscafer <giscafer@outlook.com>
2021-12-16 16:33:47 +0800

    fix: repoGitUrl

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  repoGitUrl: 'https://github.com/RootLinkFE/materials-vue/blob/master/',
+  repoGitUrl: 'https://github.com/RootLinkFE/materials-react/blob/master/',
   uploadUrl: 'https://himekaidou.souche.com/upload',
   materialsDemoUrl: 'http://materials-react.leekhub.com',
 };
